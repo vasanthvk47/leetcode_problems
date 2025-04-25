7e374b25b4d9a667da13568380013f8d6c5909bc
vasanthvk47 <vasanth472004@gmail.com>
2025-04-25 11:33:42 +0530

    Time: 12 ms (72.65%), Space: 45.6 MB (46.27%) - LeetHub

--- 2487-optimal-partition-of-string/2487-optimal-partition-of-string.java
+++ 2487-optimal-partition-of-string/2487-optimal-partition-of-string.java
@@ -1,14 +1,18 @@
 class Solution {
     public int partitionString(String s) {
         int c[]=new int[26];
-        int m=0;
+        int m=1;
         for(int i=0;i<s.length();i++)
         {
             c[s.charAt(i)-97]++;
-            if(c[s.charAt(i)-97]>1) {m++; c=new int[26]; c[s.charAt(i)-97]++;}
+            if(c[s.charAt(i)-97]>1) {
+                m++; 
+                c=new int[26]; 
+                c[s.charAt(i)-97]++;
+            }
             
         }
-        return m+1;
+        return m;
 
     }
 }
