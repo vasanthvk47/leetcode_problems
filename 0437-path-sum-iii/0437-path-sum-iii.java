3b92d32fef6f5eebb81d675fab844b74c84c49f9
vasanthvk47 <vasanth472004@gmail.com>
2025-06-24 11:32:29 +0530

    Time: 3 ms (97.25%), Space: 44.1 MB (51.9%) - LeetHub

--- 0437-path-sum-iii/0437-path-sum-iii.java
+++ 0437-path-sum-iii/0437-path-sum-iii.java
@@ -1,4 +1,4 @@
-class Solution {
+/*class Solution {
     public int pathSum(TreeNode root, int targetSum) {
         if (root == null) return 0;
         
@@ -18,6 +18,52 @@ class Solution {
 
         count += countFromNode(node.left, target - node.val);
         count += countFromNode(node.right, target - node.val);
+
+        return count;
+    }
+}
+*/
+
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public int pathSum(TreeNode root, int targetSum) {
+        if (root == null) return 0;
+        HashMap<Long,Integer> mpp = new HashMap<>();
+        mpp.put((long)0,1);
+        return dfs(root,0,(long)targetSum,mpp);
+    }
+
+    int dfs(TreeNode node,long currSum, long targetSum,HashMap<Long,Integer> mpp) {
+        if (node == null) return 0;
+        
+       currSum += node.val;
+       int count = mpp.getOrDefault(currSum - targetSum,0);
+       mpp.put((long)currSum,mpp.getOrDefault(currSum,0)+1);
+
+        // Add counts from left and right subtrees
+        count += dfs(node.left,currSum, targetSum, mpp);
+        count += dfs(node.right,currSum, targetSum, mpp);
+
+        // Backtrack
+        mpp.put(currSum,mpp.get(currSum)-1);
+        if(mpp.get(currSum) == 0){
+            mpp.remove(currSum);
+        }
 
         return count;
     }
