c5ebee31c58f9eb2da9ec4246983bdffd06fc81b
Vasanthakumar K <vasanth472004@gmail.com>
2025-12-29 20:59:05 +0530

    Time: 10 ms (81.47%), Space: 46.7 MB (66.96%) - LeetHub

--- 0438-find-all-anagrams-in-a-string/0438-find-all-anagrams-in-a-string.java
+++ 0438-find-all-anagrams-in-a-string/0438-find-all-anagrams-in-a-string.java
@@ -1,39 +1,72 @@
+// class Solution {
+//     public List<Integer> findAnagrams(String s, String p) {
+//         List<Integer> anagram_count=new ArrayList<Integer>();
+//         for(int i=0;i<s.length()-p.length()+1;i++)
+//         {
+//             if(isAnagram(s,i,p))
+//             {
+//                 anagram_count.add(i);
+//             }
+//         }
+//         return anagram_count;
+//     }
+//     public boolean isAnagram(String s,int i,String p)
+//     {
+//         int charCount[]=new int[26];
+
+//         for(int k=i;k<i+p.length();k++)
+//         {
+//             char curr_char = s.charAt(k);
+//             charCount[curr_char -'a']++;
+//         }
+
+//         for(int k=0;k<p.length();k++)
+//         {
+//             char curr_char = p.charAt(k);
+//             charCount[curr_char -'a']--;
+            
+//         }
+
+//         for(int k=0;k<26;k++)
+//         {
+//             if(charCount[k]!=0){
+//                 return false;
+//             }
+//         }
+//         return true;
+
+//     }
+// }
+
 class Solution {
     public List<Integer> findAnagrams(String s, String p) {
-        List<Integer> anagram_count=new ArrayList<Integer>();
-        for(int i=0;i<s.length()-p.length()+1;i++)
-        {
-            if(isAnagram(s,i,p))
-            {
-                anagram_count.add(i);
+        List<Integer> result = new ArrayList<>();
+        if (s.length() < p.length()) return result;
+
+        int[] pCount = new int[26]; // frequency of characters in p
+        int[] windowCount = new int[26]; // frequency of current window
+
+        // Fill frequency for p
+        for (char c : p.toCharArray()) {
+            pCount[c - 'a']++;
+        }
+
+        int windowSize = p.length();
+        for (int i = 0; i < s.length(); i++) {
+            // Add current char to the window
+            windowCount[s.charAt(i) - 'a']++;
+
+            // Remove char that is no longer in the window
+            if (i >= windowSize) {
+                windowCount[s.charAt(i - windowSize) - 'a']--;
+            }
+
+            // Compare window frequency with p's frequency
+            if (Arrays.equals(pCount, windowCount)) {
+                result.add(i - windowSize + 1);
             }
         }
-        return anagram_count;
-    }
-    public boolean isAnagram(String s,int i,String p)
-    {
-        int charCount[]=new int[26];
 
-        for(int k=i;k<i+p.length();k++)
-        {
-            char curr_char = s.charAt(k);
-            charCount[curr_char -'a']++;
-        }
-
-        for(int k=0;k<p.length();k++)
-        {
-            char curr_char = p.charAt(k);
-            charCount[curr_char -'a']--;
-            
-        }
-
-        for(int k=0;k<26;k++)
-        {
-            if(charCount[k]!=0){
-                return false;
-            }
-        }
-        return true;
-
+        return result;
     }
 }
